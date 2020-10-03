bc05b384439d07cc8f5589a3ca56a6d27e69e6dc
Angel Samaniego <171G0634@rcarbonifera.tecnm.mx>
2020-10-03 02:01:13 -0500

    subiendo archivos

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Unidad2Actividad5.Models;
+namespace Unidad2Actividad5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            animalesContext context = new animalesContext();
+            var clasificacion = context.Clase.OrderBy(x => x.Nombre);
+            if (clasificacion == null)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(clasificacion);
+            }
+        }
+        [Route("{id}")]
+        public IActionResult Clases(string id)
+        {
+            var nombre = id.Replace("-", " ").ToLower();
+            animalesContext context = new animalesContext();
+            var categ = context.Clase.Include(x => x.Especies).ThenInclude(x=>x.IdClaseNavigation).OrderBy(x => x.Nombre).FirstOrDefault(x => x.Nombre.ToLower() == nombre);
+            if (categ == null)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(categ);
+            }
+        }
+    }
+}
---
+++ Services/ClasesService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Unidad2Actividad5.Models;
+
+namespace Unidad2Actividad5.Services
+{
+    public class ClasesService
+    {
+        public List<Clase> Clases { get; set; }
+        public ClasesService()
+        {
+            using (animalesContext context = new animalesContext())
+            {
+                Clases = context.Clase.OrderBy(x => x.Nombre).ToList();
+            }
+        }
+    }
+}
